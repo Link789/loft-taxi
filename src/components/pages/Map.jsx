4560b1e8e62e6beefeafdcec07262d56c2d3b761
Link789 <33423673+Link789@users.noreply.github.com>
2022-09-01 13:06:00 +0600

    Устранение стилистических замечаний (#4)
    * Update index.js
    
    commit for pull request
    
    * Выполнено ТЗ первой недели
    
    * Доработка/исправление ТЗ первой недели
    
    11. При сабмите пользователь попадает на страницу с картой
    13. Устранены замечания наставника по ДЗ
    
    * Устранение стилистических замечаний

---
+++ src/components/pages/Map.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class MapPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                Страница Карты
+            </div>
+        )
+    }
+}
+
+export default MapPage;
---
+++ src/components/pages/Registration.jsx
@@ -0,0 +1,99 @@
+import React, {Component} from 'react';
+
+import Header from "../Header";
+import LoginPage from "./Login";
+
+import MapImg from "../../img/map_loginPage.svg";
+import LogoImg from "../../img/logo_loginPage.png";
+import '../../styles/Login.css';
+import '../../styles/Registration.css';
+
+class RegistrationPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLogin: false,
+            isRegistration: false
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        this.setState({isRegistration: true})
+    }
+
+    handleClick(event) {
+        this.setState({isLogin: true})
+    }
+
+    render() {
+        switch (true) {
+            case this.state.isRegistration:
+                return <Header page="map"/>
+            case this.state.isLogin:
+                return <LoginPage/>
+            default:
+                return (
+                    <div id='mainContainer'>
+                        <div id='leftContainer'>
+                            <img src={LogoImg} alt="Логотип"/>
+                        </div>
+                        <div
+                            id='rightContainer'
+                            style={{backgroundImage: `url(${MapImg})`}}
+                        >
+                            <form id='loginForm' onSubmit={this.handleSubmit}>
+                                <div className='formHeader'>Регистрация</div>
+
+                                <div className="container">
+                                    <label htmlFor="email"><b>Адрес электронной почты *</b></label>
+                                    <input
+                                        type="text"
+                                        placeholder="Адрес электронной почты *"
+                                        name="email"
+                                        onChange={this.handleChange}
+                                        required
+                                    />
+                                    <div className='personalData'>
+                                        <div>
+                                            <label htmlFor="firstName"><b>Имя *</b></label>
+                                            <input
+                                                type="text"
+                                                placeholder="Имя *"
+                                                name="firstName"
+                                                required
+                                            />
+                                        </div>
+                                        <div className='lastName'>
+                                            <label htmlFor="lastName"><b>Фамилия *</b></label>
+                                            <input
+                                                type="text"
+                                                placeholder="Фамилия *"
+                                                name="lastName"
+                                                required
+                                            />
+                                        </div>
+                                    </div>
+                                    <label htmlFor="password"><b>Пароль *</b></label>
+                                    <input
+                                        type="password"
+                                        placeholder="Пароль *"
+                                        name="password"
+                                        required
+                                    />
+                                    <button id='login' type="submit">Зарегистрироваться</button>
+                                </div>
+                                <div className="psw" onClick={this.handleClick}>
+                                    <span>Уже зарегистрирован?  <a href="#">Войти</a></span>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                )
+        }
+    }
+}
+
+export default RegistrationPage;
